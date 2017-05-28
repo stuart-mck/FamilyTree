e00f26eb28cbb20ea859b7e1ffcd7d13d8ecc635
heyyouau <stuart@mckenziebryce.com>
2017-05-28 10:56:36 +1000

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { HistoryComponent } from './history/history.component';
+import { TreeComponent } from './tree/tree.component';
+import { EventsComponent } from './events/events.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { LoginComponent } from './login/login.component';
+
+
+const appRoutes: Routes = [
+  { path: 'history', component: HistoryComponent },
+  { path: 'tree', component: TreeComponent },
+  { path: 'events', component: EventsComponent },
+  { path: '', pathMatch: 'full', component: FrontPageComponent },
+  { path: 'login',  component: LoginComponent },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HistoryComponent,
+    TreeComponent,
+    EventsComponent,
+    FrontPageComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  
+})
+export class AppModule { }
