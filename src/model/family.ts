ad0f7a1edbbdb588173e95359deb32acb24dc7a9
stuart-mck <stuart@mckenziebryce.com>
2017-06-12 11:59:52 +1000

    Revert "adding model classes"
    This reverts commit 71125bf2779c5734b462d53460b0c25293c676e0.

--- src/model/family.ts
+++
@@ -1,9 +0,0 @@
-import { Person } from "./person";
-
-export class Family{
-    public EntityId: number;
-    public FamilyMembers: Array<Person>;
-    constructor(entityId: number){
-        this.EntityId = entityId;
-    }
-}
